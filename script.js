9636cc235ef60a12c192714a16435af01402dc7d
Claas Paaschburg <paaschburgc@gmail.com>
2024-07-24 17:58:09 +0200

    chore: declare variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = [
+  { name: "Margherita", price: 10 },
+  { name: "Funghi", price: 12 },
+  { name: "Veggie", price: 11 },
+  { name: "Spinaci", price: 12 },
+];
+const cashInRegister = 100;
+const orderQueue = [];
