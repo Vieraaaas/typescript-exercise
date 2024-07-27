6f5e87eb85bf96e2dbdada867e5b16767fb17d42
Claas Paaschburg <paaschburgc@gmail.com>
2024-07-27 13:56:41 +0200

    docs: get rid of old task descriptions

--- script.ts
+++ script.ts
@@ -20,16 +20,10 @@ let cashInRegister = 100;
 const orderQueue: NewOrder[] = [];
 let nextOrderId = 1;
 
-// Step 1: Add a function that takes a pizza object and adds it to  the menu
 function addNewPizza(newPizza: Pizza) {
   menu.push(newPizza);
 }
 
-// Step 2: Add a function that takes a pizza name parameter and:
-//1. finds that pizza object in the menu
-//2. adds the income(price) to cashInRegister
-//3. pushes a new order object to orderQueue
-//4. returns the new order object
 function placeOrder(order: string) {
   const orderedPizza = menu.find((menuEntry) => menuEntry.name === order);
   const newOrder: NewOrder = {
@@ -47,10 +41,6 @@ function placeOrder(order: string) {
   return newOrder;
 }
 
-//Step 3: Add a function that takes an orderId as a parameter that:
-//1. finds the correct order in orderQueue
-//2. marks it's status as "completed"
-//3. returns the order
 function completeOrder(orderId: number) {
   const completedOrder = orderQueue.find(
     (orderEntry) => orderEntry.id === orderId
